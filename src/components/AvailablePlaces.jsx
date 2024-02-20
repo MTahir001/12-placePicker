ff8f1b948b8ac9d4b88cdf83276f5fb0347f9df3
MTahir01 <tahirkadgaonkar01@gmail.com>
2024-02-20 11:28:06 +0530

    make code clean, by putting fetch code in diff JS file

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -3,6 +3,7 @@ import Places from "./Places.jsx";
 import { useState } from "react";
 import Error from "./Error.jsx";
 import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -13,16 +14,10 @@ export default function AvailablePlaces({ onSelectPlace }) {
     async function fetchData() {
       setIsFetching(true);
       try {
-        const response = await fetch("http://localhost:3000/places");
-        const resData = await response.json();
-
-        if (!response.ok) {
-          throw new Error("Failed to fetch Places");
-        }
-
+        const places = await fetchAvailablePlaces();
         navigator.geolocation.getCurrentPosition((position) => {
           const sortedPlaces = sortPlacesByDistance(
-            resData.places,
+            places,
             position.coords.latitude,
             position.coords.longitude
           );
