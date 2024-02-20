2c0664ce4305cdbf7c13169783660d3486002e66
MTahir01 <tahirkadgaonkar01@gmail.com>
2024-02-20 10:16:19 +0530

    intensionally made some URL mistake and work with try-catch

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,20 +1,46 @@
 import { useEffect } from "react";
 import Places from "./Places.jsx";
 import { useState } from "react";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState("");
+
   useEffect(() => {
-    fetch("http://localhost:3000/places")
-      .then((response) => response.json())
-      .then((resData) => setAvailablePlaces(resData.places));
+    async function fetchData() {
+      setIsFetching(true);
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch Places");
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fetch places, please try again later",
+        });
+      }
+      setIsFetching(false);
+    }
+    fetchData();
   }, []);
+  if (error) {
+    return <Error title="An error occurred " message={error.message} />;
+  }
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
+      isLoading={isFetching}
+      loadingText="Fetching the data... Thanks for patience!!!"
     />
   );
 }
