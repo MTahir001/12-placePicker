1ec51ac786e7b77cbe426fb88b805824e6c3856f
MTahir01 <tahirkadgaonkar01@gmail.com>
2024-02-20 08:34:06 +0530

    fetching images from server

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,18 @@
-import Places from './Places.jsx';
+import { useEffect } from "react";
+import Places from "./Places.jsx";
+import { useState } from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/places")
+      .then((response) => response.json())
+      .then((resData) => setAvailablePlaces(resData.places));
+  }, []);
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
